58fa2de7b60558e9a22c415a3bf4762038b4980f
Nagabhushanam <nagabhushanamn@gmail.com>
2018-03-05 14:18:32 +0530

    new arith behva added

--- sample-projects/Java/calc-app/src/main/java/com/app/service/calculator/Calculator.java
+++ sample-projects/Java/calc-app/src/main/java/com/app/service/calculator/Calculator.java
@@ -1,9 +1,19 @@
 package com.app.service.calculator;
 
+/**
+ * 
+ * @author nag-devops
+ *
+ *         - added new subtract method
+ *
+ */
 public class Calculator {
 
 	public double add(double n1, double n2) {
 		return n1 + n2;
+	}
+	public double sub(double n1, double n2) {
+		return n1 - n2;
 	}
 
 }
