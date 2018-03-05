795f151ffafb8319e7eeb806e686c51c96cf0524
Nagabhushanam <nagabhushanamn@gmail.com>
2018-03-05 14:59:55 +0530

    new change

--- sample-projects/Java/calc-app/src/main/java/com/app/service/calculator/Calculator.java
+++ sample-projects/Java/calc-app/src/main/java/com/app/service/calculator/Calculator.java
@@ -10,7 +10,7 @@ package com.app.service.calculator;
 public class Calculator {
 
 	public double add(double n1, double n2) {
-		return n1 + n2;
+		return n1 * n2;
 	}
 	public double sub(double n1, double n2) {
 		return n1 - n2;
